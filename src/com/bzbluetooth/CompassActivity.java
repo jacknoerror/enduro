73d180b9dd8c51fd3c5886183a2048f131b455eb
Jack <jackinpeace@gmail.com>
2014-12-24 19:44:38 +0800

    go home complete

---
+++ src/com/bzbluetooth/CompassActivity.java
@@ -0,0 +1,16 @@
+package com.bzbluetooth;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CompassActivity extends Activity {
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_compass);
+	}
+	
+}
